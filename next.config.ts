17967c55eceb063775831f081474a8ba0db396a5
sean00880 <50418518+sean00880@users.noreply.github.com>
2024-12-25 23:16:11 -0500

    4

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Any other config options you need:
+  reactStrictMode: true,
+
+  // Ignore ESLint errors when building for production:
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  // Optionally, you can ignore TypeScript errors as well, though it's not recommended:
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
 };
 
 export default nextConfig;
