9beb2c2b7f521aed2cb9f702fc045c96ebbf687e
sean00880 <50418518+sean00880@users.noreply.github.com>
2025-01-14 00:06:55 -0500

    new 18

--- src/context/SupabaseContext.tsx
+++ src/context/SupabaseContext.tsx
@@ -2,7 +2,7 @@ import { createContext, useContext, useState, useEffect } from 'react';
 import { createClient, type AuthSession, type AuthChangeEvent, type User, type SupabaseClient, type Subscription } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_SUPABASE_ANON_KEY || '';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
